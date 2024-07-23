f2b996259b76500930c2ecf813873e2fe46ca3b5
tiagovattos <tiagovattos@gmail.com>
2024-07-23 09:27:30 -0300

    create project

---
+++ src/main/java/br/edu/fema/apidev/ApiDevApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fema.apidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevApplication.class, args);
+    }
+
+}
